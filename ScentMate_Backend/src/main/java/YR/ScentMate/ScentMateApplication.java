4e172bc75f07388e9e95997f4ff2378af5ccf10e
youngroky <139459645+youngroky@users.noreply.github.com>
2025-02-24 23:27:22 +0900

    feat : 백엔드 초기 설정 추가

---
+++ ScentMate_Backend/src/main/java/YR/ScentMate/ScentMateApplication.java
@@ -0,0 +1,13 @@
+package YR.ScentMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScentMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScentMateApplication.class, args);
+	}
+
+}
