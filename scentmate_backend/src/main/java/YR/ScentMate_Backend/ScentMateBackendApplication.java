d22f573a18296b6b5e98262f65c693a7b8036581
youngroky <139459645+youngroky@users.noreply.github.com>
2025-03-01 18:11:58 +0900

    feat: init setting

---
+++ scentmate_backend/src/main/java/YR/ScentMate_Backend/ScentMateBackendApplication.java
@@ -0,0 +1,13 @@
+package YR.ScentMate_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScentMateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScentMateBackendApplication.class, args);
+	}
+
+}
---
+++ scentmate_backend/src/test/java/YR/ScentMate_Backend/ScentMateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package YR.ScentMate_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScentMateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
